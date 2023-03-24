f8b48b08f4a314986db69462685584594ca4b419
Martin Stamm <martin.stamm@camunda.com>
2023-03-24 14:10:03 +0100

    feat: add example JSON component

---
+++ test/spec/integration.spec.js
@@ -0,0 +1,165 @@
+import TestContainer from 'mocha-test-container-support';
+
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+  ZeebePropertiesProviderModule,
+  CloudElementTemplatesPropertiesProvider
+} from 'bpmn-js-properties-panel';
+
+import ElementTemplateChooserModule from '@bpmn-io/element-template-chooser';
+
+import ZeebeBehaviorsModule from 'camunda-bpmn-js-behaviors/lib/camunda-cloud';
+import ZeebeModdle from 'zeebe-bpmn-moddle/resources/zeebe';
+
+import { ZeebeVariableResolverModule } from '@bpmn-io/variable-resolver';
+
+import VariableProviderModule from 'lib/';
+
+import Modeler from 'bpmn-js/lib/Modeler';
+
+import simpleXML from '../fixtures/simple.bpmn';
+
+// import elementTemplatesXML from '../fixtures/element-templates.bpmn';
+
+import elementTemplates from '../fixtures/templates.json';
+
+import { setBpmnJS, clearBpmnJS, insertCoreStyles, enableLogging, inject } from '../TestHelper';
+
+const singleStart = window.__env__ && window.__env__.SINGLE_START;
+
+describe('Integration', function() {
+
+  let container;
+
+  const defaultModules = [
+    ZeebeBehaviorsModule,
+    BpmnPropertiesPanelModule,
+    BpmnPropertiesProviderModule,
+    ZeebePropertiesProviderModule,
+    ZeebeVariableResolverModule,
+    VariableProviderModule
+  ];
+
+  beforeEach(function() {
+    container = TestContainer.get(this);
+  });
+
+  let modelerContainer, propertiesContainer;
+
+  beforeEach(function() {
+    insertCoreStyles();
+    modelerContainer = document.createElement('div');
+    modelerContainer.classList.add('modeler-container');
+
+    propertiesContainer = document.createElement('div');
+    propertiesContainer.classList.add('properties-container');
+
+    container = TestContainer.get(this);
+
+    container.appendChild(modelerContainer);
+    container.appendChild(propertiesContainer);
+  });
+
+  async function createModeler(xml, options = {}, BpmnJS = Modeler) {
+    const {
+      shouldImport = true,
+      additionalModules = [
+        ...defaultModules
+      ],
+      moddleExtensions = {
+        zeebe: ZeebeModdle
+      },
+      description = {},
+      layout = {}
+    } = options;
+
+    clearBpmnJS();
+
+    const modeler = new BpmnJS({
+      container: modelerContainer,
+      keyboard: {
+        bindTo: document
+      },
+      additionalModules,
+      moddleExtensions,
+      propertiesPanel: {
+        parent: propertiesContainer,
+        feelTooltipContainer: container,
+        description,
+        layout
+      },
+      ...options
+    });
+
+    setBpmnJS(modeler);
+    enableLogging(modeler, true);
+
+    if (!shouldImport) {
+      return { modeler };
+    }
+
+    try {
+      const result = await modeler.importXML(xml);
+
+      return { error: null, warnings: result.warnings, modeler: modeler };
+    } catch (err) {
+      return { error: err, warnings: err.warnings, modeler: modeler };
+    }
+  }
+
+  (singleStart ? it.only : it)('should start', async function() {
+    const result = await createModeler(simpleXML,
+      {
+        additionalModules: [
+          ZeebeBehaviorsModule,
+          BpmnPropertiesPanelModule,
+          BpmnPropertiesProviderModule,
+          ZeebePropertiesProviderModule,
+          ZeebeVariableResolverModule,
+          VariableProviderModule,
+          CloudElementTemplatesPropertiesProvider,
+          ElementTemplateChooserModule
+        ],
+        elementTemplates
+      });
+
+    expect(result.error).to.not.exist;
+  });
+
+
+  describe('variable provider', function() {
+
+    beforeEach(() => createModeler(simpleXML));
+
+    it('should supply variables to variableResolver', inject(async function(elementRegistry, variableResolver) {
+
+      // given
+      const task = elementRegistry.get('ServiceTask_1');
+
+      // when
+      const variables = await variableResolver.getVariablesForElement(task);
+
+      // then
+      expect(variables).to.variableEqual([
+        { name: 'output', type: 'Number' },
+        { name: 'startData', type: 'String' }
+      ]);
+    }));
+
+  });
+
+
+  // describe('properties panel', function() {
+
+  //   beforeEach(() => createModeler(elementTemplatesXML, {
+  //     additionalModules: [
+  //       ...defaultModules,
+  //       CloudElementTemplatesPropertiesProvider
+  //     ],
+  //     elementTemplates
+  //   }));
+
+  // });
+
+});
---
+++ test/spec/propertiesPanel.spec.js
@@ -0,0 +1,139 @@
+import TestContainer from 'mocha-test-container-support';
+
+import { bootstrapPropertiesPanel, changeInput, inject } from '../TestHelper';
+
+import { BpmnPropertiesPanelModule } from 'bpmn-js-properties-panel';
+
+import ZeebeModdle from 'zeebe-bpmn-moddle/resources/zeebe';
+
+import CoreModule from 'bpmn-js/lib/core';
+import ModelingModule from 'bpmn-js/lib/features/modeling';
+import SelectionModule from 'diagram-js/lib/features/selection';
+
+import DataPropertiesProviderModule from 'lib/propertiesPanel';
+
+import dataGroupXML from '../fixtures/dataGroup.bpmn';
+
+import {
+  act
+} from '@testing-library/preact';
+
+import {
+  query as domQuery
+} from 'min-dom';
+
+import { getExampleJson, getZeebeProperty, EXAMPLE_JSON_PROPERTY_NAME } from '../../lib/util/jsonDataUtil';
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+
+
+describe('Properties Panel - Data Group', function() {
+  const testModules = [
+    BpmnPropertiesPanelModule,
+    CoreModule,
+    ModelingModule,
+    SelectionModule,
+    DataPropertiesProviderModule
+  ];
+
+  let container;
+
+  beforeEach(function() {
+    container = TestContainer.get(this);
+  });
+
+  beforeEach(bootstrapPropertiesPanel(dataGroupXML, {
+    debounceInput: false,
+    additionalModules: testModules,
+    moddleExtensions: {
+      zeebe: ZeebeModdle
+    },
+    propertiesPanel: {
+      layout: {
+        open: true
+      }
+    }
+  }));
+
+
+  it('should display', inject(async function(elementRegistry, selection) {
+
+    // given
+    const task = elementRegistry.get('filled');
+
+    // assume
+    const exampleJSON = getExampleJson(task);
+
+    expect(exampleJSON).to.equal('{"foo": "bar"}');
+
+    // when
+    await act(() => {
+      selection.select(task);
+    });
+
+    const exampleInput = domQuery('textarea[name=exampleJson]', container);
+
+    // then
+    expect(exampleInput).to.exist;
+
+    expect(exampleInput.value).to.equal('{"foo": "bar"}');
+  }));
+
+
+  it('should apply', inject(async function(elementRegistry, selection) {
+
+    // given
+    const task = elementRegistry.get('empty');
+
+    await act(() => {
+      selection.select(task);
+    });
+    const exampleInput = domQuery('textarea[name=exampleJson]', container);
+
+    // assume
+    expect(exampleInput.value).to.equal('');
+
+    // when
+    await act(() => {
+      changeInput(exampleInput, '{"foo": "bar"}');
+    });
+
+    // then
+    expect(getExampleJson(task)).to.eql('{"foo": "bar"}');
+
+  }));
+
+
+  it('should update on external change', inject(async function(elementRegistry, selection, commandStack) {
+
+    // given
+    const task = elementRegistry.get('filled');
+    const bo = getBusinessObject(task);
+
+    await act(() => {
+      selection.select(task);
+    });
+    const exampleInput = domQuery('textarea[name=exampleJson]', container);
+
+    // assume
+    expect(exampleInput.value).to.equal('{"foo": "bar"}');
+
+    // when
+    const newValue = '{"foo": "bar", "bar": "baz"}';
+    const property = getZeebeProperty(bo, EXAMPLE_JSON_PROPERTY_NAME);
+    await act(() => {
+
+      commandStack.execute('element.updateModdleProperties', {
+        element: task,
+        moddleElement: property,
+        properties: {
+          value: newValue
+        }
+      });
+    });
+
+    // then
+    expect(getExampleJson(task)).to.eql(newValue);
+
+  }));
+
+});
